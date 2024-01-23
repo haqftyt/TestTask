bd3d3cd6438254cd4051fa5ab837b0e724ea1eec
haqftyt <103164617+haqftyt@users.noreply.github.com>
2024-01-23 14:22:52 +0530

    Update script.js

--- script/script.js
+++ script/script.js
@@ -3,7 +3,7 @@ var clickhere=document.getElementById('clickhere');
 var dayornight=document.getElementById('dayornight');
 var satellite=document.getElementById('satellite');
 clickhere.onclick=function(){
-    planet.src='https://assets.stickpng.com/images/58f9fd7b0ed2bdaf7c128329.png';
+    planet.src='img/moonImage.png';
     dayornight.innerHTML="Night";
     satellite.innerHTML="Moon";
 }

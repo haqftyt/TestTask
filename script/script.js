fad48cd11ecdce8ed58327a6a2ee1b54af6d5de4
haqftyt <fhaq4701@gmail.com>
2024-01-23 11:29:30 +0530

    MyPage is completed successfully

---
+++ script/script.js
@@ -0,0 +1,9 @@
+var planet =document.getElementById('planet');
+var clickhere=document.getElementById('clickhere');
+var dayornight=document.getElementById('dayornight');
+var satellite=document.getElementById('satellite');
+clickhere.onclick=function(){
+    planet.src='../img/moonImage.png';
+    dayornight.innerHTML="Night";
+    satellite.innerHTML="Moon";
+}
